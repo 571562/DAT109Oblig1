3fbc66d5acd56c53ced7d6ba13f1d33b28e73695
571562 <herborgisjo@hotmail.com>
2019-01-24 10:25:02 +0100

    commit

--- src/no/hvl/dat109/Terning.java
+++ src/no/hvl/dat109/Terning.java
@@ -4,22 +4,25 @@ public class Terning {
 
     public Dyr[] terning;
 
-    private Dyr løve = new Dyr("Løve", "oransje", 1);
-    private Dyr slange = new Dyr("Slange", "grønn", 2);
+    private Dyr lion = new Dyr("Løve", "oransje", 1);
+    private Dyr snake = new Dyr("Slange", "grønn", 2);
     private Dyr panda = new Dyr("Panda", "svart", 3);
-    private Dyr gris = new Dyr("Gris", "rosa", 4);
-    private Dyr elefant = new Dyr("Elefant", "grå", 5);
-    private Dyr hval = new Dyr("Hval", "blå", 6);
+    private Dyr pig = new Dyr("Gris", "rosa", 4);
+    private Dyr elephant = new Dyr("Elefant", "grå", 5);
+    private Dyr whale = new Dyr("Hval", "blå", 6);
 
 
     public Terning() {
+
         terning = new Dyr[6];
-        terning[0] = løve;
-        terning[1] = slange;
+        terning[0] = lion;
+        terning[1] = snake;
         terning[2] = panda;
-        terning[3] = gris;
-        terning[4] = elefant;
-        terning[5] = hval;
+        terning[3] = pig;
+        terning[4] = elephant;
+        terning[5] = whale;
+
+        System.out.println(lion);
 
     }
 
